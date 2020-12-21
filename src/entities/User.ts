54415a62dad45798fc641c7bfdc72964b6bd4480
Jayvee Ramos <jayvee@ag-investment.net>
2020-12-21 13:55:49 +0800

    -user save

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
+import Utility from "@shared/utility";
+import * as bcrypt from "bcrypt";
 
+
+const SALT_WORK_FACTOR = 10;
 const { Schema } = mongoose;
 export enum Role {
     guest,
     admin
 }
 
-export interface IUser {
+export interface IUser extends mongoose.Document{
     _id?: String;
     username: String;
     password: String;
@@ -18,9 +22,26 @@ export interface IUser {
 }
 
 const schema  = new Schema({
-    username: String,
-    password: String,
-    email: String,
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return Utility.validateEmail(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        },
+        required: true
+    },
     createdDate: { type: Date, default: Date.now },
     lastLogin: { type: Date, default: Date.now },
     role: {
@@ -30,6 +51,31 @@ const schema  = new Schema({
         required: true
     }
 });
+
+schema.pre<IUser>('save', function(next) {
+    var user = this;
+    // only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+schema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 const User = mongoose.model('User', schema );
 
---
+++ src/shared/utility.ts
@@ -0,0 +1,7 @@
+export default class Utility {
+
+    static validateEmail(email: any) {
+        var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return re.test(email)
+    };
+}
